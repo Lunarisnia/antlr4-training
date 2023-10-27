14beb444d5152295e0fb25483dc0db0f21d344e2
Louna <r.arswendo.r@gmail.com>
2023-10-28 02:31:37 +0700

    WIP: variable assignment

--- VisitorInterp.py
+++ VisitorInterp.py
@@ -18,3 +18,20 @@ class VisitorInterp(TrainerVisitor):
         else:
             print(f"Line {startToken.line} can't recognize Function {ctx.ID()}.")
         return super().visitFunc(ctx)
+
+    def visitVar(self, ctx: TrainerParser.VarContext):
+        for i in range(0, ctx.getChildCount()):
+            self.visit(ctx.getChild(i))
+        return 0
+    
+    def visitExpr(self, ctx: TrainerParser.ExprContext):
+        # Can be used to check if the atom exist before traveling the child
+        # print(ctx.atom())
+        if ctx.atom():
+            for i in range(0, ctx.getChildCount()):
+                self.visit(ctx.getChild(i))
+        return super().visitExpr(ctx)
+    
+    def visitAtom(self, ctx: TrainerParser.AtomContext):
+        print("ATOM")
+        return super().visitAtom(ctx)
